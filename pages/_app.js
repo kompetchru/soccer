0be961caac99f7fd99c846332bd9e48900567eff
KOMPETCH RUEANGRONG <98636@icc.co.th>
2025-03-10 11:34:56 +0700

    ปรับแต่ง UI และเพิ่ม _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>วิเคราะห์บอลวันนี้ - Soccer Analysis</title>
+                <meta name="description" content="เว็บวิเคราะห์บอลพร้อมข้อมูลทีม, ทรรศนะ และสถิติ" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;700&display=swap" rel="stylesheet" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
